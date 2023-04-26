bd4da3c559865b185110e7ba315b603a09cfe0c1
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-25 21:42:35 -0300

    Feat: Cadastro de usuário - Estrutura visual da home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,33 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          // Define o tamanho da imagem
+          height={400}
+          quality={100}
+          // Defini a imagem como prioridade no carregamento da pagina
+          priority
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,8 +1,1 @@
-import { Heading } from '@ignite-ui/react'
-
-export default function Home() {
-  return (
-    // Componente diretamente do Desing system - [ Titulo]
-    <Heading as="h1">Hello word</Heading>
-  )
-}
+export { default } from './home'
