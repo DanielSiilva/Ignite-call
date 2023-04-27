e4e5521ebf4aef21a528a0bc65e80465be9e8057
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-04-27 20:01:49 -0300

    Refactor: configurando prima

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 
+//Prima vai entender qual a conexão com o banco de dados a partir do .env
+
 export const prisma = new PrismaClient({
   log: ['query'],
 })
