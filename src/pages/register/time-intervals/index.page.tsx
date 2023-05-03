e29bbb939391c5898698ca61579a7c909f2658d6
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-02 21:47:53 -0300

    Feat: Configurando disponibilidade - Input e output do Zod

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -10,6 +10,7 @@ import {
 import { ArrowRight } from 'phosphor-react'
 import { Controller, useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { convertTimeStringToMinutes } from '../../../utils/convert-time-string-to-minutes'
 import { getWeekDays } from '../../../utils/get-week-days'
 import { Container, Header } from '../styles'
 
@@ -21,8 +22,6 @@ import {
   IntervalInputs,
   IntervalItem,
 } from './styles'
-
-// validando as entradas do formulario
 const timeIntervalsFormSchema = z.object({
   intervals: z
     .array(
@@ -34,15 +33,37 @@ const timeIntervalsFormSchema = z.object({
       }),
     )
     .length(7)
-    // filtrando apenas o check como true
     .transform((intervals) => intervals.filter((interval) => interval.enabled))
-    // validando a entrada, para que no minimo uma informação seja passada ;
     .refine((intervals) => intervals.length > 0, {
       message: 'Você precisa selecionar pelo menos um dia da semana',
-    }),
+    })
+    .transform((intervals) => {
+      return intervals.map((interval) => {
+        return {
+          weekDay: interval.weekDay,
+          startTimeInMinutes: convertTimeStringToMinutes(interval.startTime),
+          endTimeInMinutes: convertTimeStringToMinutes(interval.endTime),
+        }
+      })
+    })
+    .refine(
+      (intervals) => {
+        return intervals.every(
+          (interval) =>
+            interval.endTimeInMinutes - 60 >= interval.startTimeInMinutes,
+        )
+      },
+      {
+        message:
+          'O horário de término deve ser pelo menos 1h distante do início.',
+      },
+    ),
 })
 
-type TimeIntervalsFormData = z.infer<typeof timeIntervalsFormSchema>
+// tipagem das entradas  de dados com zod
+type TimeIntervalsFormInput = z.input<typeof timeIntervalsFormSchema>
+// tipagem das sáidas  de dados com zod
+type TimeIntervalsFormOutput = z.output<typeof timeIntervalsFormSchema>
 
 export default function TimeIntervals() {
   const {
@@ -51,7 +72,7 @@ export default function TimeIntervals() {
     control,
     watch,
     formState: { isSubmitting, errors },
-  } = useForm({
+  } = useForm<TimeIntervalsFormInput>({
     resolver: zodResolver(timeIntervalsFormSchema),
     defaultValues: {
       intervals: [
@@ -73,8 +94,10 @@ export default function TimeIntervals() {
 
   const intervals = watch('intervals')
 
-  async function handleSetTimeIntervals(data: TimeIntervalsFormData) {
-    console.log(data)
+  async function handleSetTimeIntervals(data: any) {
+    const formData = data as TimeIntervalsFormOutput
+
+    console.log(formData)
   }
 
   return (
@@ -129,12 +152,9 @@ export default function TimeIntervals() {
             )
           })}
         </IntervalContainer>
-
-        {/* Exibindo mensagem de erro, para a validação */}
         {errors.intervals && (
           <FormError size="sm">{errors.intervals.message}</FormError>
         )}
-
         <Button type="submit" disabled={isSubmitting}>
           Próximo passo
           <ArrowRight />
