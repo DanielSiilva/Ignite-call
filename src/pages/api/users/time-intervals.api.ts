7c2b8e5e1866f797b74c2d53340e7a222b4d8b20
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-05 19:34:33 -0300

    Feat: Rotas autenticadas no Next

---
+++ src/pages/api/users/time-intervals.api.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { unstable_getServerSession } from 'next-auth'
+import { buildNextAuthOptions } from '../auth/[...nextauth].api'
+
+// Rota para cadastrar time do user ;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).end()
+  }
+
+  const session = await unstable_getServerSession(
+    req,
+    res,
+    buildNextAuthOptions(req, res),
+  )
+
+  return res.json({
+    session,
+  })
+}
--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -39,7 +39,7 @@ export default function Register() {
 
   const router = useRouter()
 
-  // Busacando as informações vira parametro, usando o setValues, e sentando um novo valor atraves do paramento da url
+  // Buscando as informações vira parametro, usando o setValues, e sentando um novo valor atraves do paramento da url
   useEffect(() => {
     if (router.query.username) {
       setValue('username', String(router.query.username))
