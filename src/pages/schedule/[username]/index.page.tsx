fce7f0411d95bb231b8c8ed39929ff8ae1d92dbf
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-10 21:03:50 -0300

    Feat: Calendário e disponibilidade - Página de agendamento

---
+++ src/pages/schedule/[username]/index.page.tsx
@@ -0,0 +1,61 @@
+import { Avatar, Heading, Text } from '@ignite-ui/react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { prisma } from '../../../lib/prisma'
+import { Container, UserHeader } from './styles'
+
+interface ScheduleProps {
+  user: {
+    name: string
+    bio: string
+    avatarUrl: string
+  }
+}
+
+export default function Schedule({ user }: ScheduleProps) {
+  return (
+    <Container>
+      <UserHeader>
+        <Avatar src={user.avatarUrl} />
+        <Heading>{user.name}</Heading>
+        <Text>{user.bio}</Text>
+      </UserHeader>
+    </Container>
+  )
+}
+
+// Definir o path, para paginas que são estaticas, mas possuem parametros dinamicos
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+// fallback: usamos para que, enquanto os paramentros não forem acessados, não será carregado a pagina
+
+// Metodo para a criação de paginas estaticas
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const username = String(params?.username)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: {
+      user: {
+        name: user.name,
+        bio: user.bio,
+        avatarUrl: user.avatar_url,
+      },
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
