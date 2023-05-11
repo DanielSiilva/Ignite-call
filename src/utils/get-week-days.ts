01c0c5030066a08cf6ecf70b9495bb2ef6dc69d9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-10 21:33:23 -0300

    Feat: Calendário e disponibilidade - Estrutura visual do calendário

--- src/utils/get-week-days.ts
+++ src/utils/get-week-days.ts
@@ -1,10 +1,17 @@
-export function getWeekDays() {
+interface GetWeekDaysParams {
+  short?: boolean
+}
+
+export function getWeekDays({ short = false }: GetWeekDaysParams) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
-  //           cria um array com 7 posições e retorna um indixe com metodo keys
   return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekDay) => {
+      if (short) {
+        return weekDay.substring(0, 3).toUpperCase()
+      }
+
       return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
     })
 }
