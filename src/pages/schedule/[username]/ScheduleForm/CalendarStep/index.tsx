f9758a3f09a3c25559a1792d4bb486647a6174b9
Daniel da silva de sousa <94769388+DanielSiilva@users.noreply.github.com>
2023-05-12 21:09:12 -0300

    Feat:Calendário e disponibilidade - Tabela de agendamentos

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs'
 import { useState } from 'react'
 import { Calendar } from '../../../../../components/Calendar'
 import {
@@ -7,11 +8,15 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
-
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const describedDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -20,8 +25,9 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            terça-feira <span>20 de setembro</span>
+            {weekDay} <span>{describedDate}</span>
           </TimePickerHeader>
+
           <TimePickerList>
             <TimePickerItem>08:00h</TimePickerItem>
             <TimePickerItem>09:00h</TimePickerItem>
